a8e159037fccd593d9cbf1a42856979dc59c81e6
jubin256 <jubinjohnind@gmail.com>
2021-06-20 23:03:53 +0530

    Update documentation and code cleanup

--- bot.py
+++ bot.py
@@ -30,15 +30,23 @@ async def on_ready():
     print("Bot is available.")
 
 # Defining functions that perform certain action for a command
+# Function name should be same as the command followed by prefix
+# Eg. !Hello , so function name should be Hello
+
+# Sample command
+# We'll leave this for the actual imp as it's pretty useful to check if server
+# is live
 @client.command()
-async def Hello(ctx): ## Function name should be same as the command followed by prefix Eg. !Hello , so function name should be Hello
-    hello = hello_world()
-    await ctx.send(f' {hello}')  ## ctx.send -- to send reply from bot to Discord server
+async def Hello(ctx):
+    ## ctx.send -- to send reply from bot to Discord server
+    await ctx.send(f' Hello World')
 
 @client.command()
 # ctx is followed by parameters that are passed by user.
 # For eg - !LFG AOE playername 4
 async def LFG(ctx, gamename, player, numplayers):
+    # Currently this logic might lead to collisions in IDs but we'll live with
+    # it for now as it's incredibly unlikely with expected usage
     id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))
     await ctx.send(f' Creating request for Game : {gamename}. Looking for {numplayers} players. Group ID - {id}')
 
@@ -48,14 +56,13 @@ async def LFG(ctx, gamename, player, numplayers):
       matches = {}
       gamename_to_matches[gamename] = matches
     matches[id] = Match(gamename, id, player, numplayers)
-    await ctx.send('Done')
     await ctx.send(f'Active {gamename} games:-')
     for match_id in matches.keys():
       await ctx.send(f'{match_id}: {matches[match_id].players} {matches[match_id].numplayers}')
 
 @client.command()
-async def Join(ctx,id):
-    print("Get member id and update to player list for Group ID {id}")
+async def Join(ctx, id, player):
+    print("Adding player {player} to match ({id})")
     await ctx.send(f'Player <member-name> added to GroupID {id} , Looking for <n-1> more player(s)')
 
 
@@ -65,4 +72,4 @@ async def Leave(ctx,id):
     await ctx.send(f'Player <member-name> removed from GroupID {id} , Looking for <n-1> more player(s)')
 
 
-client.run('NzUwMTg4MzU2MDc1NTIwMDYx.X025WQ.CB3g036hYTMjWYVhkEOrpue6e6c')
+client.run('<insert token here>')
